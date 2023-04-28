388fa6a90c1ccf521f856cf0224cb6abeb7ad102
SalemChacoff <77862771+SalemChacoff@users.noreply.github.com>
2023-04-28 16:14:27 -0400

    Add player move

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+  public float moveSpeed = 1f;
+  public float collisionOffset = 0.05f;
+  public ContactFilter2D movementFilter;
+  Vector2 movementInput;
+  Rigidbody2D rb;
+  Animator animator;
+  List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private void FixedUpdate()
+  {
+    if (movementInput != Vector2.zero)
+    {
+        bool success = TryMove(movementInput);
+
+        if (!success)
+        {
+            success = TryMove(new Vector2(movementInput.x, 0f));
+            if (!success)
+            {
+                TryMove(new Vector2(0f, movementInput.y));
+            }
+        }
+
+        animator.SetBool("isMoving", success);
+    } else {
+        animator.SetBool("isMoving", false);
+    };
+  }
+
+  private bool TryMove(Vector2 direction)
+  {
+    int count = rb.Cast(
+        direction,
+        movementFilter,
+        castCollision,
+        moveSpeed * Time.fixedDeltaTime + collisionOffset
+    );
+
+    if (count == 0)
+    {
+      rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+      return true;
+    }
+
+    return false;
+  }
+
+  void OnMove(InputValue movementValue)
+  {
+    movementInput = movementValue.Get<Vector2>();
+  }
+}
